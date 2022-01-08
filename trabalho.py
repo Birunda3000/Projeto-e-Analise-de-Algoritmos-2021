d412cb8b1ad11b18859f1c032832d817babdc22c
Sara Melo <sara.melo@aluno.uece.br>
2022-01-08 18:43:04 -0300

    nomes certos e resultados

--- trabalho.py
+++ trabalho.py
@@ -2,108 +2,133 @@ import random
 import math
 
 class literal:
-
     def __init__(self, id):
         self.id = abs(id)
         self.signal = 1 if id > 0 else 0
 
-    def retornar_valor(self, sol):
-        return self.signal == sol[self.id-1]
+    def return_value(self, sol):
+        return self.signal == sol[self.id - 1]
 
-class clausula:
-    def __init__(self, id, literais):
+class clause:
+    def __init__(self, id, literals):
         self.id = id
-        self.literais = literais
+        self.literals = literals
 
-    def verificar_clausula(self, sol):
-        return (self.literais[0].retornar_valor(sol) or self.literais[1].retornar_valor(sol)) or self.literais[2].retornar_valor(sol)
+    def verify_clause(self, sol):
+        return (self.literals[0].return_value(sol) or self.literals[1].return_value(sol)) or self.literals[2].return_value(sol)
 
 class formula:
     def __init__(self):
         self.sol = []
-        self.clausulas = []
-        self.n_variaveis = 0
-        self.n_clausulas = 0
+        self.clauses = []
+        self.n_literals = 0
+        self.n_clauses = 0
 
-    def ler_arquivo(self, nome):
+    def read_txt(self, name):
         matrix = []
         aux = []
-        arq = open(nome, 'r')
-        for linha in arq:
-            linha = linha.strip()
-            linha = linha.split(" ")
-            for i in range(len(linha)):
-                aux.append( int( linha[i] ) )
+        arq = open(name, 'r')
+        for line in arq:
+            line = line.strip()
+            line = line.split(" ")
+            for i in range(len(line)):
+                aux.append(int(line[i]))
             matrix.append(aux)
             aux = []
         
-        self.n_variaveis = int(matrix[0][0])
-        self.n_clausulas = int(matrix[0][1])
+        self.n_literals = int(matrix[0][0])
+        self.n_clauses = int(matrix[0][1])
         matrix = matrix[1:]
 
-        for i in range(self.n_clausulas):
+        for i in range(self.n_clauses):
             x1 = literal(matrix[i][0])
             x2 = literal(matrix[i][1])
             x3 = literal(matrix[i][2])
-            self.clausulas.append(clausula(i , [x1,x2,x3]))
+            self.clauses.append(clause(i , [x1,x2,x3]))
   
     def initial_sol(self):
-        for _ in range(self.n_variaveis):
+        for _ in range(self.n_literals):
             self.sol.append(random.randint(0,1))
 
     def modify_sol_rand(self, half):
-        msol = self.sol.copy()
+        mod_sol = self.sol.copy()
         if half == 0:
-            m = random.randint(0, self.n_variaveis/2)
-            msol[m] = (msol[m] + 1) % 2
+            m = random.randint(0, self.n_literals/2)
+            mod_sol[m] = (mod_sol[m] + 1) % 2
         else:
-            m = random.randint(self.n_variaveis/2, self.n_variaveis-1)
-            msol[m] = (msol[m] + 1) % 2
+            m = random.randint(self.n_literals/2, self.n_literals - 1)
+            mod_sol[m] = (mod_sol[m] + 1) % 2
 
-        return msol
+        return mod_sol
 
-    def conta_verdade(self, sol):
-        v = 0
-        for clausula in self.clausulas:
-            if clausula.verificar_clausula(sol):
-                v+=1
-        return v
+    def cont_trues(self, sol):
+        t = 0
+        for clause in self.clauses:
+            if clause.verify_clause(sol):
+                t += 1
+        return t
     
-    def smanl(self, arquivo):
+    def SA(self, arq):
 
-        n_iterations1 = 40
-        n_iterations2 = 1000
+        n_its1 = 40
+        n_its2 = 1000
         T = 1000
         alpha = 0.09
 
-        self.ler_arquivo(arquivo)
+        self.read_txt(arq)
         self.initial_sol()
 
-        current_cost = self.conta_verdade(self.sol)
+        current_cost = self.cont_trues(self.sol)
 
-        for _ in range(1,n_iterations1):
+        for _ in range(1, n_its1):
 
-            for j in range(n_iterations2):
-                print(T,self.conta_verdade(self.sol))
+            for j in range(n_its2):
+                print(T, self.cont_trues(self.sol))
 
                 new_sol = self.modify_sol_rand(j%2)
-                new_cost = self.conta_verdade(new_sol)
+                new_cost = self.cont_trues(new_sol)
 
                 delta = new_cost - current_cost
 
                 if(random.random() < math.exp(delta/T)):
                     self.sol = new_sol.copy()
-                    current_cost = self.conta_verdade(self.sol)
+                    current_cost = self.cont_trues(self.sol)
 
 
             T = T * alpha
             if T < 0.005: break
 
-        print(self.conta_verdade(self.sol))
+        print(self.cont_trues(self.sol))
         print(self.sol)
-            
-
 
 
 formu = formula()
-formu.smanl('sat-1.txt')
+formu.SA('sat-1.txt')
+
+"""
+TESTES QUE DERAM O RESULTADO:
+
+272/275:
+ n_iterations1 = 40
+        n_iterations2 = 1000
+        T = 1000
+        alpha = 0.09
+
+Chegou no 499:
+
+n_iterations1 = 15
+n_iterations2 = 400
+T = 600
+alpha = 0.35
+
+Valores q mais deram certo pro sat-3:
+
+n_iterations1 = 20
+n_iterations2 = 500
+T = 700
+alpha = 0.4
+
+if T < 0.005: break
+
+
+"""
